438a6fc44c63170ed3faa46b8598a25c44e34fc0
iPakTulane <ipak.tulane@gmail.com>
2023-08-17 18:07:24 +0500

    Add docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by iPak Tulane on 17/08/23.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
