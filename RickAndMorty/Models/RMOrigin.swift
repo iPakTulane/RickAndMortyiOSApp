da172bbfb11bd24de4f3993bd97fa7ee02599edc
iPakTulane <ipak.tulane@gmail.com>
2023-08-20 14:56:29 +0500

    Add models

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by iPak Tulane on 18/08/23.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
